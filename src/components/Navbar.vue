d6c587e18703c22976883e28f0bb82c0106b78ef
Mo'men Soliman <moumensoliman@gmail.com>
2018-12-20 01:13:27 +0200

    Add navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,71 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <div class="d-flex flex-column flex-md-row align-items-center mb-3 ">
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="navbar-brand my-0 mr-md-auto font-weight-normal" href="#">
+          <img src="@/assets/images/home/png-icons/seen-logo@3x.png"
+          class="d-inline-block align-top"
+          alt="Sceen Logo">
+        </a>
+        <ul>
+          <li><a class="p-2 text-dark" href="#">تواصل معنا</a></li>
+          <li><a class="btn btn-outline-primary" href="#">الملف التعريفي <img src="@/assets/images/home/svg-icons/cloud-storage-download.svg" alt="sceen-cloud-storage-dodwnload" /></a></li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Navbar',
+  name: 'Navbar'
 };
 </script>
 
 <style lang="scss">
-
+nav {
+  width: 100%;
+  padding: 38px 0;
+  ul {
+    float: left;
+    padding: 0;
+    li {
+      display: inline-block;
+      list-style: none;
+      margin-right: 36px;
+      a {
+        font-size: 14px;
+        &:hover {
+          text-decoration: none;
+        }
+        &.btn {
+          width: 145px;
+          height: 40px;
+          border-radius: 20px;
+          background-color: #1f9eab;
+          color: white;
+          border: 0;
+          font-size: 14px;
+          &:hover {
+            opacity: 0.8;
+            background-color: #1f9eab;
+          }
+          img {
+            width: 20px;
+            height: 15px;
+            object-fit: contain;
+            margin-left: 7px;
+          }
+        }
+      }
+    }
+  }
+  .navbar-brand {
+    img {
+      width: 120px;
+      object-fit: contain;
+      margin-top: 10px;
+    }
+  }
+}
 </style>
