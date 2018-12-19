76c674ac2b048a981017d2e2fe98176d7309775e
Mo'men Soliman <moumensoliman@gmail.com>
2018-12-19 23:47:01 +0200

    Add project base

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div class="main-container">
+      <div class="container">
+        <router-view/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+//base.scss file contain variables, website skeleton
+@import './assets/style/base.scss';
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
+#app {
+  font-family: $font-family;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style lang="scss">
+
+</style>
