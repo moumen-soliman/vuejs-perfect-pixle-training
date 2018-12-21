b4eedf6667e97bc923b133d449002f27df78e228
Mo'men Soliman <moumensoliman@gmail.com>
2018-12-21 03:22:48 +0200

    Create flow button

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <router-view :userInfo="userInfo"/>
+    <FlowButton />
   </div>
 </template>
 
 <script>
+import FlowButton from './components/common/FlowButton';
+
 export default {
   name: 'App',
+  components: {
+    FlowButton
+  },
   data() {
     return {
       userInfo: {
@@ -20,7 +26,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto');
+@import url('https://fonts.googleapis.com/css?family=Roboto:300i,400,400i,500,500i,700,700i');
 @import '../node_modules/bootstrap/scss/bootstrap.scss';
 body {
   background: #f5f7fa;
