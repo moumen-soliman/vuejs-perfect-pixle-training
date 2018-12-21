f40f88e6222080cfc3005d3243f52da13ba00acd
Mo'men Soliman <moumensoliman@gmail.com>
2018-12-21 03:25:41 +0200

    Create AddCard component

---
+++ src/components/common/Tasks/AddCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="col add-card">
+      <span>Add card</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddCard',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.add-card {
+  text-align: center;
+  padding: 21px;
+  border-radius: 6px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+  margin: 10px;
+  width: 100%;
+  span {
+    font-size: 14px;
+    color: #d7d6d6;
+  }
+}
+
+@media screen and (max-width: 767px) {
+  .add-card {
+    margin: 10px 0;
+  }
+}
+</style>
