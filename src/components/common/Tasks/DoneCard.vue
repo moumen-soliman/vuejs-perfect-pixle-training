fdc049f64adf49498fca57464fd13061b8acc700
Mo'men Soliman <moumensoliman@gmail.com>
2018-12-21 03:25:51 +0200

    Create DoneCard

---
+++ src/components/common/Tasks/DoneCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="done-block" :style="{background: `${jobsColor}`}">
+      <span>Done</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DoneCard',
+  props: ['jobsColor'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.done-block {
+  text-align: center;
+  width: 100%;
+  border-radius: 6px;
+  position: relative;
+  padding: 42px;
+  span {
+    margin: auto;
+    color: white;
+  }
+}
+</style>
